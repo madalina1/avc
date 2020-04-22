1bccd4de77bcb6d2a628b2c9f4344cc7f3b54a56
robertdanielh <robert.herscovici@geminicad.com>
2020-04-22 23:30:26 +0300

    small clean up

--- app/app/src/main/java/com/example/avcapp/selfTesting/CustomView.kt
+++ app/app/src/main/java/com/example/avcapp/selfTesting/CustomView.kt
@@ -1,26 +1,34 @@
 package com.example.avcapp.selfTesting
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import com.google.firebase.ml.vision.common.FirebaseVisionPoint
 
 class CustomView @JvmOverloads constructor(context: Context,
                                            attrs: AttributeSet? = null, defStyleAttr: Int = 0)
     : View(context, attrs, defStyleAttr) {
 
-    public fun setCol(col :Int) {
-        squarePaint = Paint().apply {
-            isAntiAlias = true
-            color = col
-            style = Paint.Style.FILL
-        }
+    var mouthLeft: Point = Point()
+    var mouthRight: Point = Point()
+    var mouthBottom: Point = Point()
+    var noseBase: Point = Point()
+
+    public fun setPosition( mouthLeft : FirebaseVisionPoint     ,
+                            mouthRigth : FirebaseVisionPoint    ,
+                            mouthBottom : FirebaseVisionPoint   ,
+                            noseBase: FirebaseVisionPoint       ) {
+        this.mouthLeft = transformToCanvasCoords(mouthLeft)
+        this.mouthRight = transformToCanvasCoords(mouthRigth)
+        this.mouthBottom = transformToCanvasCoords(mouthBottom)
+        this.noseBase = transformToCanvasCoords(noseBase)
     }
 
-
+    private fun transformToCanvasCoords(fbPoint : FirebaseVisionPoint): Point {
+        //todo
+        return Point()
+    }
 
     // Called when the view should render its content.
     override fun onDraw(canvas: Canvas) {
@@ -28,8 +36,6 @@ class CustomView @JvmOverloads constructor(context: Context,
         canvas.drawRect(Rect(0,0, 100, 100), squarePaint)
         // DRAW STUFF HERE
     }
-
-
 
     companion object {
         var squarePaint =
